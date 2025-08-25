89c313dba452363b0481f809e2c27a30909e9e48
parth-kachariya <parthkachariya668@gmail.com>
2025-08-25 17:18:01 +0530

    Add Files

---
+++ project-1/script.js
@@ -0,0 +1,25 @@
+// Basic calculation
+{
+
+    let a = 10;
+    let b = 20;
+  console.log("Addition:", a + b);
+  console.log("Multiplication:", a * b);
+  console.log("Division:", a / b);
+  console.log("Subtraction:", a - b);
+  console.log("Modulus:", a % b);
+  console.log("Exponentiation:", a ** 2);
+  console.log("Post-increment:", a++);
+  console.log("Pre-increment:", ++b);
+  console.log("Post-decrement:", a--);
+  console.log("Pre-decrement :", --b);
+
+  console.log("Assignment operators");
+  
+  console.log("Addition:", a += b);
+  console.log("Multiplication:", a *= b);
+  console.log("Division:", a /= b);
+  console.log("Subtraction:", a -= b);
+  console.log("Modulus:", a %= b);
+  console.log("Exponentiation:", a **= 2);
+}
